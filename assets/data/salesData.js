536d61eaab03b5c8a89e88f2d4d414f2166de6ca
unknown <srijandas18.sd@gmail.com>
2022-10-26 12:28:15 +0530

    transaction tab done

---
+++ assets/data/salesData.js
@@ -0,0 +1,12 @@
+export default salesData = [
+  {
+    name: "Mosh",
+    total: 177,
+    balance: 0,
+  },
+  {
+    name: "John",
+    total: 500,
+    balance: 100,
+  },
+];
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,13 @@
 import {
   StyleSheet,
   View,
-  Image,
   StatusBar,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import { FAB, Tab, Text, TabView } from "@rneui/themed";
+import { FAB, Tab, Text, TabView, ListItem, Button, Icon } from "@rneui/themed";
 import { useState } from "react";
+import { salesData } from "../assets/data";
 
 const Home = ({ navigation }) => {
   const [visible, setVisible] = useState(true);
@@ -38,14 +39,73 @@ const Home = ({ navigation }) => {
 
       <TabView value={index} onChange={setIndex} animationType="spring">
         <TabView.Item style={{ width: "100%" }}>
-          <Text h1>Transactions</Text>
+          <ScrollView>
+            {salesData.map((sale, i) => (
+              <TouchableOpacity key={i}>
+                <ListItem bottomDivider>
+                  <ListItem.Content>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: "100%",
+                      }}
+                    >
+                      <ListItem.Title style={{ fontWeight: "bold" }}>
+                        {sale.name}
+                      </ListItem.Title>
+                      <View style={{ alignItems: "flex-end" }}>
+                        <Text style={{ color: "#808080" }}>
+                          {new Date().toLocaleDateString()}
+                        </Text>
+                        <Text style={{ color: "#808080" }}>#{i + 1}</Text>
+                      </View>
+                    </View>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        width: "100%",
+                        marginTop: 5,
+                      }}
+                    >
+                      <View>
+                        <Text style={{ color: "#808080", fontSize: 12 }}>
+                          Total
+                        </Text>
+                        <Text style={{ fontSize: 15 }}>₹ {sale.total}</Text>
+                      </View>
+                      <View>
+                        <Text style={{ color: "#808080", fontSize: 12 }}>
+                          Balance
+                        </Text>
+                        <Text style={{ fontSize: 15 }}>₹ {sale.balance}</Text>
+                      </View>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          alignItems: "center",
+                        }}
+                      >
+                        <TouchableOpacity style={{ marginRight: 20 }}>
+                          <Icon name="print" color="#808080" type="ionicons" />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                          <Icon name="share" color="#808080" type="ionicons" />
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </ListItem.Content>
+                </ListItem>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         </TabView.Item>
         <TabView.Item style={{ width: "100%" }}>
           <Text h1>Items</Text>
         </TabView.Item>
       </TabView>
-      {/* <View style={styles.container}>
-        <Image style={styles.tinyLogo} source={require("../assets/logo.png")} /> */}
 
       <FAB
         visible={visible}
@@ -55,23 +115,10 @@ const Home = ({ navigation }) => {
         title="New Bill"
         onPress={() => navigation.navigate("CreateBill")}
       />
-      {/* </View> */}
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  tinyLogo: {
-    width: 100,
-    height: 100,
-    opacity: 0.1,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default Home;
